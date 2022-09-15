734b6c76e9240466c4e51589b3feed1c701d6c50
Ghunt2021 <89885988+Ghunt2021@users.noreply.github.com>
2022-09-14 23:06:30 -0400

    Major Updated
    Mold, Red, and Green apples drop, the tree moves, and the tree drops apples

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -1,18 +1,127 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Set in Inspector")]
+
+    // Prefab for instantiating apples
+
+    public GameObject applePrefab;
+    public GameObject applePrefabG;
+    public GameObject applePrefabM;
+
+    // Speed at which the AppleTree moves
+
+    public float speed = 1f;
+
+
+
+    // Distance where AppleTree turns around
+
+    public float leftAndRightEdge = 10f;
+
+
+
+    // Chance that the AppleTree will change directions
+
+    public float chanceToChangeDirections = 0.1f;
+
+
+
+    // Rate at which Apples will be instantiated
+
+    public float secondsBetweenAppleDrops = 1f;
+
+    public float chanceForRed = 0.7f;
+    public float chanceForGreen = 0.5f;
+    public float chanceForMold = 1f;
+
+
+
     void Start()
     {
-        
+        // Dropping apples every second
+
+        Invoke("DropApple", 2f);
+        // Dropping apples every second
+
     }
 
-    // Update is called once per frame
+    void DropApple()
+    {                                                  // b
+
+        if (Random.value < chanceForRed)
+        {
+            GameObject appleR = Instantiate<GameObject>(applePrefab);      // c
+            appleR.transform.position = transform.position;                  // d
+        }
+
+        if (Random.value > chanceForRed && Random.value < chanceForGreen)
+        { 
+            GameObject appleG = Instantiate<GameObject>(applePrefabG);      // c
+            appleG.transform.position = transform.position;                  // d
+        }
+        
+        if (Random.value > chanceForRed && Random.value > chanceForGreen)
+        {
+        GameObject appleM = Instantiate<GameObject>(applePrefabM);      // c
+        appleM.transform.position = transform.position;                  // d
+        }
+
+        Invoke("DropApple", secondsBetweenAppleDrops);                // e
+
+    }
+
     void Update()
     {
-        
+
+        // Basic Movement
+
+        Vector3 pos = transform.position;                  // b
+
+        pos.x += speed * Time.deltaTime;                   // c
+
+        transform.position = pos;                          // d
+
+        if (pos.x < -leftAndRightEdge)
+        {
+
+            speed = Mathf.Abs(speed);  // Move right
+
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+
+            speed = -Mathf.Abs(speed); // Move left
+
+        }
+        else if (Random.value < chanceToChangeDirections)
+        {        // a
+
+            speed *= -1; // Change direction                           // b
+
+        }
+
     }
+
+    void FixedUpdate()
+    {
+
+        // Changing Direction Randomly is now time-based because of FixedUpdate()
+
+        if (Random.value < chanceToChangeDirections)
+        {                     // b
+
+            speed *= -1; // Change direction
+
+        }
+
+    }
+
+
 }
--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,18 +1,102 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class MouseMovement : MonoBehaviour
+
+
+public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Set in Inspector")]                                             // a
+
+    public GameObject basketPrefab;
+
+    public int numBaskets = 3;
+
+    public float basketBottomY = -14f;
+
+    public float basketSpacingY = 2f;
+
+
+
     void Start()
     {
-        
+
+        for (int i = 0; i < numBaskets; i++)
+        {
+
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+
+            Vector3 pos = Vector3.zero;
+
+            pos.y = basketBottomY + (basketSpacingY * i);
+
+            tBasketGO.transform.position = pos;
+
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        // Get the current screen position of the mouse from Input
+
+        Vector3 mousePos2D = Input.mousePosition;                             // a
+
+
+
+        // The Camera's z position sets how far to push the mouse into 3D
+
+        mousePos2D.z = -Camera.main.transform.position.z;                     // b
+
+
+
+        // Convert the point from 2D screen space into 3D game world space
+
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);    // c
+
+
+
+        // Move the x position of this Basket to the x position of the Mouse
+
+        Vector3 pos = this.transform.position;
+
+        pos.x = mousePos3D.x;
+
+        this.transform.position = pos;
+
     }
+
+    void OnCollisionEnter(Collision coll)
+    {                         // a
+
+        // Find out what hit this basket
+
+        GameObject collidedWith = coll.gameObject;                    // b
+
+        if (collidedWith.tag == "Apple")
+        {                          // c
+
+            Destroy(collidedWith);
+
+        }
+
+        //if (collidedWith.tag == "AppleG")
+        //{                          // c
+
+        //    Destroy(collidedWith);
+
+        //}
+
+        //if (collidedWith.tag == "MoldApple")
+        //{                          // c
+
+        //    Destroy(collidedWith);
+
+        //}
+    }
+
 }
